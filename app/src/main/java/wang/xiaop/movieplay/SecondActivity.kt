57d2989ded8763a62661f5ddacf621b6a7f333da
王攀 <pan_king@foxmail.com>
2017-08-22 18:31:59 +0800

    Init

---
+++ app/src/main/java/wang/xiaop/movieplay/SecondActivity.kt
@@ -0,0 +1,12 @@
+package wang.xiaop.movieplay
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
